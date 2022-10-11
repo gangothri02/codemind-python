349d3da7abdad6a09520a680782735d6f323b0ba
gangothri02 <81913259+gangothri02@users.noreply.github.com>
2022-10-11 06:25:19 +0530

    n=int(input()) l=list(map(int,input().split())) k=int(input()) s=0 e=0 for i in l:     c=0     for j in range(1,i+1):         if i%j==0:             c+=1     if c==2:         if i<=k:             s+=1 print(s)

---
+++ primes_less_than_k.py
@@ -0,0 +1,14 @@
+n=int(input())
+l=list(map(int,input().split()))
+k=int(input())
+s=0
+e=0
+for i in l:
+    c=0
+    for j in range(1,i+1):
+        if i%j==0:
+            c+=1
+    if c==2:
+        if i<=k:
+            s+=1
+print(s)
