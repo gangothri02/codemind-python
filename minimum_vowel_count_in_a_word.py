df2e95ef296e46ef12d4d90d5dd9c025b63acb52
gangothri02 <81913259+gangothri02@users.noreply.github.com>
2022-10-14 07:40:41 +0530

    s=input() s=s.split() l=[] for i in s:     c=0     for j in i:         if ord(j)<97:             m=ord(j)+32             if m==97 or m==101 or m==111 or m==105 or m==117:                 c+=1         else:             if ord(j)==97 or ord(j)==101 or ord(j)==111 or ord(j)==105 or ord(j)==117:                 c+=1     l.append(c) print(min(l))

---
+++ minimum_vowel_count_in_a_word.py
@@ -0,0 +1,15 @@
+s=input()
+s=s.split()
+l=[]
+for i in s:
+    c=0
+    for j in i:
+        if ord(j)<97:
+            m=ord(j)+32
+            if m==97 or m==101 or m==111 or m==105 or m==117:
+                c+=1
+        else:
+            if ord(j)==97 or ord(j)==101 or ord(j)==111 or ord(j)==105 or ord(j)==117:
+                c+=1
+    l.append(c)
+print(min(l))
